af5abe75a5f3e49288c635bd086b4f61e51abe9b
Alfredo G Ceballos <agceballos@cpp.edu>
2017-09-27 21:39:04 -0700

    Client can send server a message

--- EchoClient.java
+++ EchoClient.java
@@ -1,17 +1,29 @@
-
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.InputStream;
-import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public final class EchoClient {
 
     public static void main(String[] args) throws Exception {
+		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+		String msg = "";
         try (Socket socket = new Socket("localhost", 22222)) {
+			//while(true) {
+				
+			String address = socket.getInetAddress().getHostAddress();
             InputStream is = socket.getInputStream();
             InputStreamReader isr = new InputStreamReader(is, "UTF-8");
             BufferedReader br = new BufferedReader(isr);
             System.out.println(br.readLine());
+			
+			OutputStream os = socket.getOutputStream();
+            PrintStream out = new PrintStream(os, true, "UTF-8");
+            out.printf("Hi server! This is %s", address);
+			//}
         }
     }
 }
--- EchoServer.java
+++ EchoServer.java
@@ -1,6 +1,15 @@
-
+/*****************************************
+*
+*
+*
+*
+*
+*****************************************/
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -15,6 +24,11 @@ public final class EchoServer {
                     OutputStream os = socket.getOutputStream();
                     PrintStream out = new PrintStream(os, true, "UTF-8");
                     out.printf("Hi %s, thanks for connecting!%n", address);
+					
+					InputStream is = socket.getInputStream();
+					InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+					BufferedReader br = new BufferedReader(isr);
+					System.out.println(br.readLine());
                 }
             }
         }
