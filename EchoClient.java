ee33824b0ce76e2b0fea7f501613de3bcb330a4c
Alfredo G Ceballos <agceballos@cpp.edu>
2017-09-29 12:48:41 -0700

    Finished and added commenting

--- EchoClient.java
+++ EchoClient.java
@@ -1,3 +1,10 @@
+/*****************************************
+*	Alfredo Ceballos
+*	CS 380 - Computer Networks
+*	Excercise 1
+*	Professor Nima Davarpanah
+*****************************************/
+
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.InputStream;
@@ -9,30 +16,47 @@ import java.net.Socket;
 public final class EchoClient {
 
     public static void main(String[] args) throws Exception {
+		// BufferedReader for keyboard
 		BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
-		String msg = "";
+		String msg = "";	// Used to hold input
+		
+		//  Initialize new socket and connect with server
+		Socket socket = new Socket("localhost", 22222);
+		
+		/*
+		 * Everytime a new instance of the EchoClient program is created,
+		 * that instance only needs to create input and output streams once
+		 * rather than everytime the following while loop begins.
+		 */
+		 
+		//Creating input and output streams for the client
+		String address = socket.getInetAddress().getHostAddress();
+		InputStream is = socket.getInputStream();
+		InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+						
+		OutputStream os = socket.getOutputStream();
+		PrintStream out = new PrintStream(os, true, "UTF-8");
+		
 		while(true){
-			try (Socket socket = new Socket("localhost", 22222)) {
-					
-				String address = socket.getInetAddress().getHostAddress();
-				InputStream is = socket.getInputStream();
-				InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-				BufferedReader br = new BufferedReader(isr);
-				System.out.println(br.readLine());
-				
-				OutputStream os = socket.getOutputStream();
-				PrintStream out = new PrintStream(os, true, "UTF-8");
-				//out.printf("Hi server! This is %s", address);
+			try {
+				System.out.print("Client> ");
 				msg = kb.readLine();
+				
 				if(msg.toLowerCase().equals("exit")){
 					System.exit(0);
 				}
-				//System.out.printf("Sending %s%n",msg);//delete
-				out.printf("%s",msg);
 				
+				out.printf("%s\n",msg);
+				System.out.println(br.readLine());
 			}
-		}
+			catch(Exception e){
+				System.out.println("Something went wrong...");
+			}
+		}// Ending of while loop
+		
     }
+	
 }
 
 
--- EchoServer.java
+++ EchoServer.java
@@ -1,10 +1,10 @@
 /*****************************************
-*
-*
-*
-*
-*
+*	Alfredo Ceballos
+*	CS 380 - Computer Networks
+*	Excercise 1
+*	Professor Nima Davarpanah
 *****************************************/
+
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.IOException;
@@ -17,26 +17,64 @@ import java.net.Socket;
 public final class EchoServer {
 
     public static void main(String[] args) throws Exception {
+		
+		// Initiallize server socket
         try (ServerSocket serverSocket = new ServerSocket(22222)) {
+			
+			// Infinite loop
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    //System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-					
-					InputStream is = socket.getInputStream();
-					InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-					BufferedReader br = new BufferedReader(isr);
-					try{
-						System.out.println(br.readLine());
+				// Accept connections
+				Socket socket = serverSocket.accept();
+				
+				/*
+				 *	Creating Runnable thread for server. 
+				 * 	Thread begins right after an EchoClient server process establishes
+				 *	a connection with the server, meaning multiple EchoClient processes and
+				 *	have independent threads dedicated to handling them.
+				 */
+				Runnable echoServerThread = () -> {
+					try {
+						// Save client's address
+						String address = socket.getInetAddress().getHostAddress();
+						System.out.printf("Client connected: %s%n", address);
+						
+						// Create outputstream and printstream to send messages to clients
+						OutputStream os = socket.getOutputStream();
+						PrintStream out = new PrintStream(os, true, "UTF-8");
+						
+						// Inputstream and bufferedreader to receive clients' messages
+						InputStream is = socket.getInputStream();
+						InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+						BufferedReader clientBr = new BufferedReader(isr);
+						
+						/*
+						 * This while loop will continously wait for input from the client
+						 * coming in through the inputstream's bufferedreader. Once it receives
+						 * something it will send it back. If the input is "exit" then the loop
+						 * breaks and the socket is closed right after.
+						 */
+						while(true) {
+							String clientMsg = clientBr.readLine();	// Save client message
+							out.printf("Server> %s%n",clientMsg);	// Send message back
+							//System.out.println(clientMsg);	// Checking message
+							if(clientMsg.equals("exit")){
+								break;
+							}
+						}
+						
+						socket.close();
+						System.out.println("socket closed");
 					}
 					catch(IOException e){
-						System.out.println("A client has disconnected...");
+							System.out.println("A client has disconnected...");
 					}
-                }
-            }
+				};	// Ending of echoServerThread
+				
+				new Thread(echoServerThread).start();
+			}	//Ending or loop
+			
         }
+		
     }
+	
 }
